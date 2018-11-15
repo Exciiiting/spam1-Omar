11375a4faa7bccc243a09cdcb53e2b7364a7f2d9
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-11-15 12:37:42 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,14 @@ const client = new Discord.Client();
 
 
 
+client.on('message', message => {
+if (message.content === "test") {
+  var ms = 120000;
+      var interval = setInterval (function () {
+        message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبرلا إله الا الله, سبحان الله, الحمدالله, الله اكبرلا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
+      }, ms); 
+    }
+});
 
 
  
